6eaa6754e0af632e48a232c49c9e2a5c249eac62
Aleksey Lobanov <i@likemath.ru>
2017-10-04 19:46:24 +0300

    Updates from manin repo

---
+++ src/Version.cpp
@@ -0,0 +1,13 @@
+#include <afina/Version.h>
+
+#include <string>
+
+namespace Afina {
+
+const std::string Version_Major = "";
+const std::string Version_Minor = "";
+const std::string Version_Patch = "";
+const std::string Version_Release = "";
+const std::string Version_SHA =  "";
+
+} // namespace Afina
