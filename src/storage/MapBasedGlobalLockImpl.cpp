d9d39da888ab0bc4eaf51bedb9eb600f58180c66
Aleksey Lobanov <i@likemath.ru>
2017-10-06 02:25:13 +0300

    hw-2 fixes

--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -14,7 +14,6 @@ bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &valu
 	if ( _to_remove.size() > _max_size ) {
 		Delete(_to_remove.front());
 	}
-
     _backend[key] = value;
     return true;
 }
@@ -24,15 +23,18 @@ bool MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::stri
     std::unique_lock<std::mutex> guard(_lock);
     if ( _backend.find(key) == _backend.end() )
 		return Put(key, value);
-	return false;
+	return true;
 }
 
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::Set(const std::string &key, const std::string &value) {
     std::unique_lock<std::mutex> guard(_lock);
-    if ( _backend.find(key) != _backend.end() )
+    if ( _backend.find(key) != _backend.end() ) {
 		return Put(key, value);
-    return Put(key, value);
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // See MapBasedGlobalLockImpl.h
@@ -42,16 +44,23 @@ bool MapBasedGlobalLockImpl::Delete(const std::string &key) {
     if ( pos != _backend.end() )
 		_backend.erase(key);
 	auto to_remove_pos = find(_to_remove.begin(), _to_remove.end(), key);
-	if ( to_remove_pos != _to_remove.end() )
+	if ( to_remove_pos != _to_remove.end() ) {
 		_to_remove.erase(to_remove_pos);
-    return true;
+		return true;
+	} else
+		return false;
 }
 
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) const {
     std::unique_lock<std::mutex> guard(*const_cast<std::mutex *>(&_lock));
-    value = _backend.at(key);
-    return true;
+
+    auto pos = _backend.find(key);
+    if ( pos != _backend.end() ) {
+		value = pos->second;
+        return true;
+    }
+    return false;
 }
 
 } // namespace Backend
